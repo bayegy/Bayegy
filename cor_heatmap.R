cbf8456a96e013d9b6627779bc642503bc3f3e81
bayegy <947366452@qq.com>
2018-08-22 11:38:43 +0800

    cor

---
+++ cor_heatmap.R
@@ -0,0 +1,32 @@
+library("optparse")
+library("pheatmap")
+library(psych)
+#######arguments
+option_list <- list( 
+    make_option(c("-i", "--input"), dest="otu",help="Specify the path of collapsed bacteria table",default=NULL),
+    make_option(c("-m", "--map"),dest="map", help="Specify the path of mapping file",default=NULL),
+    make_option(c("-e", "--exclude"),dest="ex", help="Specify the numeric variables excluded from plot and seprated by commas in mapping file",default="none"),
+    make_option(c("-n", "--number"), dest="num",help="How many species do you want to plot",default=20),
+    make_option(c("-o", "--output"),dest="out", help="Specify the path of output files",default="./")
+    )
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+########prepare the cor data
+ex<-str_split(opt$ex,",")[[1]]
+dat <- read.table(opt$input,comment.char="", header = TRUE, sep = "\\t")
+dat<-dat[!duplicated(dat[,1]),]
+
+rownames(dat)=dat[,1]
+map<-read.table("%s",header = T,row.names=1,comment.char = "",check.names = F,stringsAsFactors = F)
+groups<-map["%s"]
+notstr=c()
+for(i in 1:length(map)){
+	notstr[i]=!is.character(map[,i])
+}
+envdata<-map[,notstr]
+if(ex[1]!="none"){envdata<-envdata[,!colnames(envdata)%%in%%ex]}
+
+dat=dat[,-c(1,length(dat))]
+dat=t(dat)[match(rownames(groups),rownames(t(dat))),]
+
