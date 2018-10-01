77806fcac1aae4db401ba8c19949ecf6db5c9732
bayegy <947366452@qq.com>
2018-10-01 12:46:29 +0800

    script for filter and correct batch effects

--- de_batch_effect.R
+++ de_batch_effect.R
@@ -1,7 +1,7 @@
 library('optparse')
 option_list <- list(
-	make_option(c("-i", "--input"),metavar="path", dest="data",help="Specify the abundance table you want to debatch, you should not contain any comment in the file, taxonomy or otuid must in the firsr as well as last column",default=NULL),
-	make_option(c('-f',"--feature-table-taxonomy"),metavar="path", dest="otu",help="Specify the otu table you want to debatch with comment at first row, if --input is supplied, this one will be ignored.",default=NULL),
+	make_option(c("-i", "--feature-table-with-comment"),metavar="path", dest="data",help="Specify the abundance table you want to debatch, you should not contain any comment in the file, taxonomy or otuid must in the firsr as well as last column",default=NULL),
+	make_option(c('-f',"--feature-table-without-comment"),metavar="path", dest="otu",help="Specify the otu table you want to debatch with comment at first row, if --input is supplied, this one will be ignored.",default=NULL),
   make_option(c("-m", "--map"),metavar="path",dest="map", help="Specify the path of mapping file",default=NULL),
   make_option(c("-c", "--category"),metavar="string",dest="group", help="Specify category name in mapping file, the effect of which will be eliminated. If not passed, no batch effect will be corrected. You can specify more than one category, use commas as delimeter",default=NULL),
 	make_option(c("-e", "--exclude"),metavar="string",dest="ex", help="Specify the numeric variables excluded from batch effect correcting. Use commas as delimiter",default="none"),
@@ -14,7 +14,7 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list=option_list))
 
 library(stringr)
-library(sva)
+
 if(!dir.exists(opt$out)){dir.create(opt$out,recursive = T)}
 
 if(!is.null(opt$data)){
@@ -40,41 +40,43 @@ if(!is.null(opt$frequency)){
   data<-data[sel,]
   backdata<-backdata[sel,]
 }
+N<-0
 if (!is.null(opt$group)){
+  library(sva)
   map<-read.table(opt$map,comment.char="",check.names=F,stringsAsFactors=F, header = TRUE, sep = "\t",na.strings='')
+  map<-map[,-ncol(map)]
   map<-map[match(colnames(data),map[,1]),]
   notstr=c()
-  for(i in 1:length(map)){
+  for(i in 1:ncol(map)){
     notstr[i]=!is.character(map[,i])
   }
   ex<-str_split(opt$ex,",")[[1]]
   if(ex[1]!="none"){notstr<-(notstr&(!colnames(map)%in%ex))}
-  batch_map<-t(map[,notstr])
+  N<-sum(notstr)
+  batch_map<-t(data.frame(map[,notstr],check.names = F))
   colnames(batch_map)<-map[,1]
   map<-map[,!notstr]
   for (group in str_split(opt$group,',')[[1]]) {
     batch <- as.factor(map[group][,1])
     modcombat = model.matrix(~1, data=map)
     data <- ComBat(dat=data, batch=batch, mod=modcombat, par.prior=F, prior.plots=F,mean.only = as.logical(opt$onlymean))
-    batch_map <- ComBat(dat=batch_map, batch=batch, mod=modcombat, par.prior=F, prior.plots=F,mean.only = as.logical(opt$onlymean))
-  
+    if(N>0){batch_map <- ComBat(dat=batch_map, batch=batch, mod=modcombat, par.prior=F, prior.plots=F,mean.only = as.logical(opt$onlymean))}
   }
+  batch_map<-t(batch_map)
 }
-batch_map<-t(batch_map)
+
 data<-data.frame(backdata[,1],data,backdata[,2],check.names = F)
 colnames(data)[c(1,length(data))]<-colname_backdata
 
-
-
 if(!is.null(opt$data)){
-  write.table(data,paste(opt$out,'/','Batch_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F)
+  write.table(data,paste(opt$out,'/','Filtered_or_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F)
 }else{
-  cm<-"#Feature table filterd or debatched"
-  write.table(cm,paste(opt$out,'/','Batch_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F,col.names=F,append = F)
-  write.table(data,paste(opt$out,'/','Batch_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F,append = T)
+  cm<-"# Feature table filterd or batch effects corrected"
+  write.table(cm,paste(opt$out,'/','Filtered_or_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F,col.names=F,append = F)
+  suppressWarnings(write.table(data,paste(opt$out,'/','Filtered_or_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F,append = T))
 }
 
-if (!is.null(opt$group)){
-  write.table(data.frame(map,batch_map,check.names = F,check.rows = T),paste(opt$out,'/','Batch_corrected_map.txt',sep = ''),sep = '\t',quote = F,row.names = F)
+if (!is.null(opt$group)&N>0){
+  write.table(data.frame(map,batch_map,Description=map[,1],check.names = F,check.rows = F),paste(opt$out,'/','Batch_corrected_map.txt',sep = ''),sep = '\t',quote = F,row.names = F)
 }
 
