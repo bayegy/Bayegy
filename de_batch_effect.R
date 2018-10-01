987bbfde1fd2d4e758abdc65a45e66dce76408f0
unknown <947366452@qq.com>
2018-10-02 01:54:09 +0800

    none

--- de_batch_effect.R
+++ de_batch_effect.R
@@ -2,11 +2,12 @@ library('optparse')
 option_list <- list(
 	make_option(c("-i", "--input"),metavar="path", dest="data",help="Specify the abundance table you want to debatch, you should not contain any comment in the file, taxonomy or otuid must in the firsr as well as last column",default=NULL),
 	make_option(c('-f',"--feature-table-taxonomy"),metavar="path", dest="otu",help="Specify the otu table you want to debatch with comment at first row, if --input is supplied, this one will be ignored.",default=NULL),
-    make_option(c("-m", "--map"),metavar="path",dest="map", help="Specify the path of mapping file",default=NULL),
-    make_option(c("-c", "--category"),metavar="string",dest="group", help="Specify category name in mapping file, the effect of which will be eliminated. If not passed, no batch effect will be corrected",default=NULL),
-    make_option(c("--min-samples"),metavar="int or float",dest="sample", help="Pass this to filter your otu table before debatch",default=NULL),
-    make_option(c("--min-frequency"),metavar="int or float",dest="frequency", help="Pass this to filter your otu table before debatch",default=NULL),
-    make_option(c("--only-mean"),metavar='logical',dest="onlymean", help="If TRUE, only mean will be corrected, otherwise both variance and mean will be corrected",default=TRUE),
+  make_option(c("-m", "--map"),metavar="path",dest="map", help="Specify the path of mapping file",default=NULL),
+  make_option(c("-c", "--category"),metavar="string",dest="group", help="Specify category name in mapping file, the effect of which will be eliminated. If not passed, no batch effect will be corrected. You can specify more than one category, use commas as delimeter",default=NULL),
+	make_option(c("-e", "--exclude"),metavar="string",dest="ex", help="Specify the numeric variables excluded from batch effect correcting. Use commas as delimiter",default="none"),
+	make_option(c("--min-samples"),metavar="int or float",dest="sample", help="Pass this to filter your otu table before debatch",default=NULL),
+  make_option(c("--min-frequency"),metavar="int or float",dest="frequency", help="Pass this to filter your otu table before debatch",default=NULL),
+  make_option(c("--only-mean"),metavar='logical',dest="onlymean", help="If TRUE, only mean will be corrected, otherwise both variance and mean will be corrected",default=TRUE),
 	make_option(c("-o", "--output"),metavar="directory",dest="out", help="Specify the directory where the debatched abundance file will be placed",default="./")  
 	)
 
@@ -14,6 +15,7 @@ opt <- parse_args(OptionParser(option_list=option_list))
 
 library(stringr)
 library(sva)
+if(!dir.exists(opt$out)){dir.create(opt$out,recursive = T)}
 
 if(!is.null(opt$data)){
   data<- read.table(opt$data,comment.char="",check.names=F,stringsAsFactors=F, header = TRUE, sep = "\t",na.strings='')
@@ -39,23 +41,40 @@ if(!is.null(opt$frequency)){
   backdata<-backdata[sel,]
 }
 if (!is.null(opt$group)){
-  map<-read.table(opt$map,row.names = 1,comment.char="",check.names=F,stringsAsFactors=F, header = TRUE, sep = "\t",na.strings='')
-  map<-map[match(colnames(data),rownames(map)),]
-  batch <- as.factor(map[opt$group][,1])
-  modcombat = model.matrix(~1, data=map)
-  data <- ComBat(dat=data, batch=batch, mod=modcombat, par.prior=F, prior.plots=F,mean.only = as.logical(opt$onlymean))
+  map<-read.table(opt$map,comment.char="",check.names=F,stringsAsFactors=F, header = TRUE, sep = "\t",na.strings='')
+  map<-map[match(colnames(data),map[,1]),]
+  notstr=c()
+  for(i in 1:length(map)){
+    notstr[i]=!is.character(map[,i])
+  }
+  ex<-str_split(opt$ex,",")[[1]]
+  if(ex[1]!="none"){notstr<-(notstr&(!colnames(map)%in%ex))}
+  batch_map<-t(map[,notstr])
+  colnames(batch_map)<-map[,1]
+  map<-map[,!notstr]
+  for (group in str_split(opt$group,',')[[1]]) {
+    batch <- as.factor(map[group][,1])
+    modcombat = model.matrix(~1, data=map)
+    data <- ComBat(dat=data, batch=batch, mod=modcombat, par.prior=F, prior.plots=F,mean.only = as.logical(opt$onlymean))
+    batch_map <- ComBat(dat=batch_map, batch=batch, mod=modcombat, par.prior=F, prior.plots=F,mean.only = as.logical(opt$onlymean))
+  
+  }
 }
-
+batch_map<-t(batch_map)
 data<-data.frame(backdata[,1],data,backdata[,2],check.names = F)
 colnames(data)[c(1,length(data))]<-colname_backdata
 
+
+
 if(!is.null(opt$data)){
-  write.table(data,opt$out,sep = '\t',quote = F,row.names = F)
+  write.table(data,paste(opt$out,'/','Batch_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F)
 }else{
   cm<-"#Feature table filterd or debatched"
-  write.table(cm,opt$out,sep = '\t',quote = F,row.names = F,col.names=F,append = F)
-  write.table(data,opt$out,sep = '\t',quote = F,row.names = F,append = T)
+  write.table(cm,paste(opt$out,'/','Batch_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F,col.names=F,append = F)
+  write.table(data,paste(opt$out,'/','Batch_corrected_feature_table.txt',sep = ''),sep = '\t',quote = F,row.names = F,append = T)
 }
 
-
+if (!is.null(opt$group)){
+  write.table(data.frame(map,batch_map,check.names = F,check.rows = T),paste(opt$out,'/','Batch_corrected_map.txt',sep = ''),sep = '\t',quote = F,row.names = F)
+}
 
