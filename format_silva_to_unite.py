e3ed2552cc5c46b7dca48481bba07d3647b30a10
bayegy <947366452@qq.com>
2018-11-11 14:05:37 +0800

    add the script to format silva taxonomy

--- format_silva_to_unite.py
+++ format_silva_to_unite.py
@@ -5,9 +5,11 @@ import os
 # argument:
 p = argparse.ArgumentParser()
 p.add_argument('-u', '--unite-taxonomy', dest='unite', metavar='txt',
-               help='its taxonomy')
+               help='unite taxonomy')
 p.add_argument('-s', '--silva-taxonomy', dest='silva', metavar='txt',
                help='silva taxonomy')
+p.add_argument('-c', '--clean_unclassified', dest='clean', action='store_true',
+               help='Supply this to clean the unclassified levels')
 p.add_argument('-o', '--out-path', dest='out', metavar='path',
                help='out path')
 
@@ -15,54 +17,56 @@ p.add_argument('-o', '--out-path', dest='out', metavar='path',
 options = p.parse_args()
 
 
-phylums = []
-classes = []
+unites = []
 
 with open(options.unite, 'r') as unite:
     for line in unite:
-        li = re.search('[^\t]+$', line).group().strip()
-        phylums.append(re.search('p__([^;]*)', li).group(1).strip())
-        classes.append(re.search('c__([^;]*)', li).group(1).strip())
+        li = re.split('\t *', line)
+        unites.append(li[1])
 
-phylums = list(set(phylums))
-classes = list(set(classes))
+levels = ['k__', 'p__', 'c__', 'o__', 'f__', 'g__', 's__']
 
 
-def find_level(tar, level='p'):
-    if level == 'p':
-        for phylum in phylums:
-            if re.search('__' + phylum + ' *;', tar):
-                return('p__' + phylum)
-        return('')
+def clean_tax(tax):
+    for ambi in ['Incertae', 'Ambiguous', 'uncultured', 'unclassified', 'unidentified']:
+        tax = re.sub(';[^;]*' + ambi + '[^\n]*', '', tax)
+    return(tax)
 
-    elif level == 'c':
-        for clas in classes:
-            if re.search('__' + clas + ' *;', tar):
-                ll = re.search('__' + clas + ' *;.*', tar).group().strip()
-                for ambi in ['Incertae', 'Ambiguous', 'uncultured', 'unclassified', 'unidentified']:
-                    ll = re.sub('[^;]*' + ambi + '.*',
-                                'unidentified;unidentified;unidentified;unidentified;unidentified', ll)
-                ll = re.split(';', ll)[0:5]
-                ll = [re.sub('^.*__', '', l.strip()) for l in ll]
-                tll = ''
-                for cp in zip(['c__', 'o__', 'f__', 'g__', 's__'], ll):
-                    tll = tll + ';' + cp[0] + cp[1]
-                tll = re.sub(' ', '_', tll)
-                return(tll)
-        return('')
+
+def merge_tax(tax):
+    if re.search('D_3__Fungi', tax):
+        hind_tax = []
+        tax = re.search('D_3__Fungi.*', tax).group()
+        tax = re.sub('unidentified', '', tax)
+        tax = re.sub('D_\d+__ *[;|\n].*', '', tax)
+        tax = re.sub('D_\d+__', '', tax)
+        tax = re.split(';', tax)
+        tax = [l.strip() for l in tax]
+        for ta in reversed(tax):
+            ta = re.sub(' ', '_', ta)
+            hind_tax = [ta] + hind_tax
+            for ref in unites:
+                if re.search('__' + ta + ' *[;|\n]', ref):
+                    pre_tax = re.search('(^.*__' + ta + ')' + ' *[;|\n]', ref).group(1)
+                    del hind_tax[0]
+                    if len(hind_tax) > 0:
+                        hind_tax = hind_tax + ['unidentified', 'unidentified',
+                                               'unidentified', 'unidentified', 'unidentified', 'unidentified']
+                        n_l = 7 - len(re.split(';', pre_tax))
+                        f_hind_tax = []
+                        for ite in zip(levels[-n_l:], hind_tax[:n_l]):
+                            f_hind_tax.append(ite[0] + ite[1])
+                        return(pre_tax + ';' + ';'.join(f_hind_tax) + '\n')
+                    else:
+                        return(pre_tax + '\n')
+    else:
+        return(tax)
 
 
 with open(options.silva, 'r') as silva, open(options.out, 'w') as out:
     for line in silva:
-        line = re.split('\t', line)
-        tax = line[1].strip()
-        if re.search('D_3__Fungi', tax):
-            tax = re.search('D_3__Fungi.*', tax).group()
-            found_phlum = find_level(tax, level='p')
-            if found_phlum:
-                found_class = find_level(tax, level='c')
-                out.write(line[0] + '\t' + 'k__Fungi;' + found_phlum + found_class + '\n')
-            else:
-                out.write(line[0] + '\t' + 'k__Fungi' + '\n')
-        else:
-            out.write(line[0] + '\t' + 'Unassigned' + '\n')
+        li = re.split('\t', line)
+        taxonomy = merge_tax(li[1])
+        if options.clean:
+            taxonomy = clean_tax(taxonomy)
+        out.write(li[0] + '\t' + taxonomy)
