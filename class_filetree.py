92aa2bbabba82e2d588f90d081c2fa5cc77ae95a
unknown <947366452@qq.com>
2019-01-23 15:28:08 +0800

    update filetree

--- class_filetree.py
+++ class_filetree.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import re,sys,os
 import argparse
 class classfiletree(object):
@@ -15,7 +16,7 @@ class classfiletree(object):
 	def get_tree(self):
 		os.system("tree /F %s > %s/raw_filetree.txt"%(self.inpath,self.outpath))
 	
-	def align_filetree(self,raw_tree,length=82):
+	def align_filetree(self,raw_tree,length=100):
 		n=int(length)
 		fout=open(self.alignedfiletree,'w')
 		for line in open(raw_tree):
@@ -26,29 +27,33 @@ class classfiletree(object):
 		fout.close()
 
 	def inherit_filetree(self,father,mask):
+		print(mask)
 		mask=re.split(',',mask)
+		mask=[re.compile(i) for i in mask]
+		print(mask)
 		ann={}
-		for line in open(father,'r'):
-			line=re.sub('^[^0-9a-zA-Z_]+','',line)
-			line=re.sub('\n$','',line)
-			for i in mask:
-				line=re.sub(i,'',line)
+		for line in open(father,'r',encoding='utf-8'):
+			line=re.sub('^[^0-9a-zA-Z_]+','',line.strip())
 			line=re.sub('Between_[^_]+_and_[^_]+','',line)
 			line=re.split('·+',line)
-			#print(line)
-			try:
-				ann[line[0]]=line[1]
-			except IndexError:
-				pass
+			for i in mask:
+				line[0]=re.sub(i,'',line[0])
+			#print(line[0])
+			if not line[0]=="":
+				try:
+					if not (line[0] in ann.keys() or line[1]==""):
+						ann[line[0]]=line[1]
+				except IndexError:
+					pass
 
 		with open(self.child_filetree,'w') as fout:
 			for line in open(self.alignedfiletree,'r'):
-				line=re.sub('\n$','',line)
-				li=re.sub('^[^0-9a-zA-Z_]+','',line)
+				line=line.strip()
+				li=re.split('·+',line)[0]
+				li=re.sub('^[^0-9a-zA-Z_]+','',li)
+				li=re.sub('Between_[^_]+_and_[^_]+','',li)
 				for i in mask:
 					li=re.sub(i,'',li)
-				li=re.sub('Between_[^_]+_and_[^_]+','',li)
-				li=re.sub('·+$','',li)
 				#print(li)
 				try:
 					fout.write(line+ann[li]+'\n')
@@ -61,18 +66,17 @@ class classfiletree(object):
 		n=1
 		for line in open(dirfiletree):
 			if n in l:
-				if re.search('·+[^·]{1,100}\n$',line)!=None:
+				if re.search('·+[^·]+\n$',line)!=None:
 					fout.write(line)
 					n=n+1
-				elif re.search(' +$',line)!=None:
+				elif re.search('[ |│]+$',line)!=None:
 					l.append(n+1)
 					n=n+1
 			else:
-				if re.search('·+[^·]{1,100}\n$',line)!=None:
+				if re.search('·+[^·]+\n$',line)!=None:
 					fout.write(line)
 					n=n+1
-				elif re.search(' +$',line)!=None:
-					
+				elif re.search('[ |│]+$',line)!=None:
 					fout.write(line)
 					l.append(n+1)
 					n=n+1
@@ -88,7 +92,7 @@ if __name__=="__main__":
 	p.add_argument('-f', '--father-filetree', dest = 'father', metavar = '<path>',
 				help = 'the filetree to be inherited')
 	p.add_argument('-m', '--mask', dest = 'mask', metavar = '<str>',
-				help = 'Strings in filename to be masked when aligning. Use comma as delimeter',default='Group')
+				help = 'Strings in filename to be masked when aligning. Use comma as delimeter',default='Group\d*')
 	options = p.parse_args()
 	ex=classfiletree(father_filetree=options.father,inpath=options.input,outpath=options.output)
 	ex.get_tree()
